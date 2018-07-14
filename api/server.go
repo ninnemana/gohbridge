cb0ba0aa4c648a7afa90b7a93750c378ed98455e
Alex Ninneman <alex@ninneman.org>
2018-07-13 23:07:48 -0500

    sample server

--- api/server.go
+++ api/server.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/ninnemana/gohbridge/hue/bridge"
+	bridgeSvc "github.com/ninnemana/gohbridge/hue/bridge/service"
+	"github.com/ninnemana/gohbridge/hue/lights"
+	lightSvc "github.com/ninnemana/gohbridge/hue/lights/service"
+	"github.com/ninnemana/gohbridge/logger"
+	"google.golang.org/grpc/grpclog"
+
+	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/trace"
-	bridgepb "github.com/ninnemana/gohbridge/hue/bridge/service"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -19,6 +26,8 @@ const (
 func main() {
 	ctx := context.Background()
 	tc, err := trace.NewClient(ctx, "ninneman-org")
+
+	bq, err := bigquery.NewClient(ctx, "ninneman-org")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,12 +39,35 @@ func main() {
 
 	grpc.EnableTracing = true
 
-	s := grpc.NewServer(grpc.UnaryInterceptor(tc.GRPCServerInterceptor()))
-	bridgepb.RegisterHueServer(s, &bridgepb.Service{})
-	reflection.Register(s)
+	s := grpc.NewServer(
+		grpc.StreamInterceptor(
+			grpc_middleware.ChainStreamServer(
+				logger.StreamServerInterceptor(bq, logger.Option{}),
+				// tc.StreamServerInterceptor(),
+			),
+		),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				logger.UnaryServerInterceptor(bq, logger.Option{}),
+				tc.GRPCServerInterceptor(),
+			),
+		),
+	)
 
-	fmt.Printf("starting RPC server on %s ...\n", port)
+	lg := grpclog.NewLoggerV2(os.Stdout, os.Stderr, os.Stderr)
+	grpclog.SetLoggerV2(lg)
+
+	// register bridge service
+	bridge.RegisterServiceServer(s, &bridgeSvc.Service{
+		Log:   lg,
+		Trace: tc,
+	})
+
+	// register light service
+	light.RegisterServiceServer(s, &lightSvc.Service{})
+
+	grpclog.Infof("starting RPC server on %s ...\n", port)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to start RPC server: %s", err.Error())
+		grpclog.Fatalf("failed to start RPC server: %v", err)
 	}
 }
